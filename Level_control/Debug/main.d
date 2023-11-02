d39752b5575ded90abc1b960ba36c90073c56267
CyberTheFox <Borazon.vb@gmail.com>
2023-11-02 20:00:40 +0300

    Add files via upload

---
+++ Level_control/Debug/main.d
@@ -0,0 +1,30 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: G:/dev_soft/ccs/ccs_base/msp430/include/msp430.h
+main.obj: G:/dev_soft/ccs/ccs_base/msp430/include/msp430g2553.h
+main.obj: G:/dev_soft/ccs/ccs_base/msp430/include/in430.h
+main.obj: G:/dev_soft/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h
+main.obj: G:/dev_soft/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h
+main.obj: G:/dev_soft/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stdbool.h
+main.obj: G:/dev_soft/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/_ti_config.h
+main.obj: G:/dev_soft/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/linkage.h
+
+../main.c:
+
+G:/dev_soft/ccs/ccs_base/msp430/include/msp430.h:
+
+G:/dev_soft/ccs/ccs_base/msp430/include/msp430g2553.h:
+
+G:/dev_soft/ccs/ccs_base/msp430/include/in430.h:
+
+G:/dev_soft/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h:
+
+G:/dev_soft/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h:
+
+G:/dev_soft/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stdbool.h:
+
+G:/dev_soft/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/_ti_config.h:
+
+G:/dev_soft/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/linkage.h:
+
